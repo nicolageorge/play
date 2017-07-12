61e7df9acebb2aed9403fa20842f9d7754b1e5f7
George Nicola <george.nicola@1and1.ro>
2017-07-12 19:09:26 +0300

    adding refs

---
+++ __init__.py
@@ -0,0 +1,11 @@
+# http://www.geeksforgeeks.org/commonly-asked-data-structure-interview-questions-set-1/
+# http://www.geeksforgeeks.org/data-structures/
+
+# miahi
+# https://stackoverflow.com/questions/3255/big-o-how-do-you-calculate-approximate-it
+# https://en.wikipedia.org/wiki/Big_O_notation#Orders_of_common_functions
+
+# mine
+# https://en.wikipedia.org/wiki/Best,_worst_and_average_case
+# https://en.wikipedia.org/wiki/Computational_complexity_theory
+# http://bigocheatsheet.com/
